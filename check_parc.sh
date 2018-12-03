731229b349fcc872be9dd09de9d90e3b803641fb
Jonathan Tay <jonathantay193@gmail.com>
2018-12-03 15:41:15 +0000

    load aseg if no aparc found

--- check_parc.sh
+++ check_parc.sh
@@ -6,4 +6,12 @@ module load freesurfer
 
 orig=$subj/mri/orig.mgz
 parc=$subj/mri/aparc+aseg.mgz
-freeview $orig $parc:colormap=lut:opacity=0.5
+aseg=$subj/mri/aseg.auto.mgz
+
+if [ -e $parc ]; then
+  freeview $orig $parc:colormap=lut:opacity=0.5
+else
+  echo "Aparc not found, loading aseg"
+  freeview $orig $aseg:colormap=lut:opacity=0.5
+fi
+
