dbe9b69da3a54771198663394acf3c6f01c5ac75
Jonathan Tay <jonathantay193@gmail.com>
2018-12-07 14:23:13 +0000

    added provision if no aseg

--- check_parc.sh
+++ check_parc.sh
@@ -7,11 +7,15 @@ module load freesurfer
 orig=$subj/mri/orig.mgz
 parc=$subj/mri/aparc+aseg.mgz
 aseg=$subj/mri/aseg.auto.mgz
+brainmask=$subj/mri/brainmask.mgz
 
 if [ -e $parc ]; then
   freeview $orig $parc:colormap=lut:opacity=0.5
-else
-  echo "Aparc not found, loading aseg"
+elif [ -e $aseg ]; then
+  echo "Aparc not found"
   freeview $orig $aseg:colormap=lut:opacity=0.5
+else 
+  echo "Aseg not found"
+  freeview $orig $brainmask
 fi
 
