e2a65518d8e351965f625ddecc89a02a2a6579d6
Jonathan Tay <jonathantay193@gmail.com>
2019-11-10 22:01:02 +0000

    made new destrieux

--- info/read.py
+++ info/read.py
@@ -1,11 +1,14 @@
 # import pandas as pd
 
 x = []
+i = 0
 with open('destrieux.txt') as f:
     for line in f:
         row = line.strip().split()
+        row[0] = str(i)
         x.append('\t'.join(row))
+        i+=1
 
-# with open('destrieux_new.txt', 'w+') as f:
-#     f.write('\n'.join(x))
-#     f.write('\n')
+with open('destrieux_new.txt', 'w+') as f:
+    f.write('\n'.join(x))
+    f.write('\n')
