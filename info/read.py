12ee003f92bb32826f3091155017a6e775c049f0
Jonathan Tay <jonathantay193@gmail.com>
2019-11-10 21:55:10 +0000

    added destrieux and scripts to convert

---
+++ info/read.py
@@ -0,0 +1,11 @@
+# import pandas as pd
+
+x = []
+with open('destrieux.txt') as f:
+    for line in f:
+        row = line.strip().split()
+        x.append('\t'.join(row))
+
+with open('destrieux_new.txt', 'w+') as f:
+    f.write('\n'.join(x))
+    f.write('\n')
