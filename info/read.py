e89c8785226c6653fd23fd7cd89cea6c02d140a7
Jonathan Tay <jonathantay193@gmail.com>
2019-11-12 18:42:00 +0000

    squash! commented first lines

--- info/read.py
+++ info/read.py
@@ -2,13 +2,13 @@
 
 x = []
 i = 0
-with open('destrieux.txt') as f:
+with open('aparc.a2009s+aseg.txt') as f:
     for line in f:
         row = line.strip().split()
         row[0] = str(i)
         x.append('\t'.join(row))
         i+=1
 
-with open('destrieux_new.txt', 'w+') as f:
+with open('aparc.a2009s+aseg_new.txt', 'w+') as f:
     f.write('\n'.join(x))
     f.write('\n')
